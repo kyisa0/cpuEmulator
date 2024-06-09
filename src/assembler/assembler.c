8287326e75d1c2c162aa1f76c0d752938ba951f9
Kerem <106799208+kyisa0@users.noreply.github.com>
2024-06-09 12:43:11 +0300

    Add files via upload

---
+++ src/assembler/assembler.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+
+int main(int argc, char *argv[])
+{
+    if(!(argc>1))
+    {
+        printf("Usage: [file.s] [options]");
+        return 1;
+    }
+}
